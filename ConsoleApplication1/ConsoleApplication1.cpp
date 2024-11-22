72ff0f4fa59149001e11e8b5a34b5d3cb17b154e
contest <contest@W12-414-0000006>
2024-11-22 14:44:08 +0400

    update

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,122 +1,112 @@
-﻿#include <locale.h>
-#include <time.h>
-#include <vector>
-#include <deque>
-#include <list>
-#include <algorithm>
-#include <ppl.h>
+﻿#include <agents.h>
+#include <cmath>
 #include <iostream>
-#include <cmath> 
-#define NNN 1200
+#include <locale.h>
+#include <time.h>
 
-double TaskOne(int x) {
-    double ans = 0;
-    int end = std::max(20, 20 * std::abs(x));
-    for (int k = 1; k <= end; k++) {
-        for (int j = 1; j <= end; j++) {
+using namespace concurrency;
+using namespace std;
 
-            double denominator = x * x + k * k  + j * j;
-            if (denominator != 0) {
-                ans += ((x*x) / denominator) *  cos((k+j) * x);
-            }
-        }
-    }
-    return ans;
+int const N = 50; //Размерность рядов
+int const NNN = 500; //Длина массива на входе конвейера 
+int const Chunk = 100; //Порция обрабатываемых элементов
+
+double Func1(double x)//Первая ступень конвейера
+{
+	double totalSum = 0;
+	for (int n = 0; n <= N; ++n) {
+		for (int k = 0; k <= N; ++k) {
+			for (int j = 0; j <= N; ++j) {
+				double denominator = 1 + sqrt(pow(x, 2) + pow(n, 4)) + pow(k, 2) + pow(j, 3);
+				if (denominator != 0) {  // Проверка на деление на ноль
+					totalSum += (x + j) / denominator;
+				}
+			}
+		}
+	}
+	return totalSum;
 }
 
-double My_Task(double x) {
-    return TaskOne(static_cast<int>(x)); // Приведение к int, если нужно
+double Func2(double x)//Вторая ступень конвейера
+{
+	double totalSum = 0;
+	for (int n = 0; n <= N; ++n) {
+		for (int k = 0; k <= N; ++k) {
+			for (int j = 0; j <= N; ++j) {
+				double numerator = pow(x, 2) - k;
+				double denominator = 1 + pow(x, 2) + pow(n, 3) + pow(k, 3) + pow(j, 2);
+
+				if (denominator != 0) {  // Проверка на деление на ноль
+					totalSum += numerator / denominator;
+				}
+			}
+		}
+	}
+	return totalSum;
 }
 
+class Stage_Agent : public agent
+{
+private:
+	// Буфер - источник информации
+	ISource<double>& _source;
+	// Буфер - приемник информации
+	ITarget<double>& _target;
+	//Функция для обработки
+	double(*_func) (double);
+public:
+	Stage_Agent(ISource<double>& source, ITarget<double>& target, double func(double))
+		: _target(target), _source(source)
+	{
+		_func = func;
+	}
+protected:
+	void run()
+	{
+		while (true) {
+			asend(_target, _func(receive(_source)));
+		}
+
+	}
+
+};
 
 int main()
 {
-    setlocale(LC_ALL, ".ACP");
-    double V1[NNN];
-    double Time = clock();
-    std::cout << "Сравнение последовательной версии и версии на основе concurrency::parallel_for:\n";
+	setlocale(LC_ALL, ".ACP");
+	cout << "Конвейер на основе блоков сообщений и программных агентов:" << endl;
+	vector<double> X(NNN), Y(NNN), Z(NNN);
+	//Заполнение входного вектора
+	for (int k = 0; k < X.size(); k++)
+		X[k] = 10 * sin((double)k);
+	double Tms = clock();
+	//Последовательные вычисления
+	for (int k = 0; k < X.size(); k++) {
+		Y[k] = Func1(X[k]);
+		Y[k] = Func2(Y[k]);
+	}
+	Tms = (clock() - Tms) / CLOCKS_PER_SEC;
+	cout << "- Время последовательного алгоритма: " << Tms << " c." << endl;
+	unbounded_buffer<double> buf0, buf1, buf2;
+	//Первая ступень конвейера	
+	Stage_Agent Stage_Agent1(buf0, buf1, Func1);
+	//Вторая ступень конвейера
+	Stage_Agent Stage_Agent2(buf1, buf2, Func2);
+	Stage_Agent1.start();
+	Stage_Agent2.start();
 
-    for (int k = 0; k < NNN; k++) {
-        V1[k] = My_Task(100 * cos(k));
-    }
+	Tms = clock();
+	//Параллельные вычисления
+	for (int k = 0; k < Chunk; k++)
+		send(buf0, X[k]);
 
-
-    // сравнение времени    вычисления сумм
-    Time = (clock() - Time) / CLOCKS_PER_SEC;
-    std::cout << "Подзадачи завершены" << std::endl
-        << "- Время выполнения последовательной версии: " << Time << " сек." << std::endl;
-
-    Time = clock();
-    Concurrency::parallel_for(0, NNN, [&V1](size_t k) { V1[k] = My_Task(100 * cos(k)); });
-
-    Time = (clock() - Time) / CLOCKS_PER_SEC;
-    std::cout << "Подзадачи завершены" << std::endl
-        << "- Время выполнения параллельной версии: " << Time << " сек." << std::endl;
-
-
-
-    // сравнение эффективности для vector
-    std::vector<double> V(NNN); // Создаём вектор
-    for (int k = 0; k < NNN; k++)
-        V[k] = 100 * cos(k);
-    std::vector<double> VP(V); // Конструируем копию
-
-    std::cout << "\nСравнение std::for_each и parallel_for_each для вектора:\n";
-
-    Time = clock();
-    std::for_each(V.begin(), V.end(),
-        [](double& x) {x = My_Task(x); });
-    Time = (clock() - Time) / CLOCKS_PER_SEC;
-    std::cout << "Подзадачи завершены" << std::endl
-        << "- Время посл. обр: " << Time << " сек." << std::endl;
-    Time = clock();
-    concurrency::parallel_for_each(VP.begin(), VP.end(), [](double& x) {x = My_Task(x); });
-    Time = (clock() - Time) / CLOCKS_PER_SEC;
-    std::cout << "Подзадачи завершены" << std::endl
-        << "- Время парал. обр.: " << Time << " сек." << std::endl;
-
-    // сравнение эффективности для deque
-    std::deque<double> Dq;
-    for (int k = 0; k < NNN; k++)
-        Dq.push_back(100 * cos(k));
-    std::deque<double> DqP(Dq);
-
-    std::cout << "\nСравнение std::for_each и parallel_for_each для дека:\n";
-
-    Time = clock();
-    std::for_each(Dq.begin(), Dq.end(),
-        [](double& x) {x = My_Task(x); });
-    Time = (clock() - Time) / CLOCKS_PER_SEC;
-    std::cout << "Подзадачи завершены" << std::endl
-        << "- Время посл. обр: " << Time << " сек." << std::endl;
-    Time = clock();
-    concurrency::parallel_for_each(DqP.begin(), DqP.end(),
-        [](double& x) {x = My_Task(x); });
-    Time = (clock() - Time) / CLOCKS_PER_SEC;
-    std::cout << "Подзадачи завершены" << std::endl
-        << "- Время парал. обр.: " << Time << " сек." << std::endl;
-
-    // сравнение эффективности для list
-    std::list<double> Lst;
-    for (int k = 0; k < NNN; k++)
-        Lst.push_back(100 * cos(k));
-    std::list<double> LstP(Lst);
-
-    std::cout << "\nСравнение std::for_each и parallel_for_each для списка:\n";
-
-    Time = clock();
-    std::for_each(Lst.begin(), Lst.end(),
-        [](double& x) {x = My_Task(x); });
-    Time = (clock() - Time) / CLOCKS_PER_SEC;
-    std::cout << "Подзадачи завершены" << std::endl
-        << "- Время посл. обр: " << Time << " сек." << std::endl;
-    Time = clock();
-    concurrency::parallel_for_each(LstP.begin(), LstP.end(),
-        [](double& x) {x = My_Task(x); });
-    Time = (clock() - Time) / CLOCKS_PER_SEC;
-    std::cout << "Подзадачи завершены" << std::endl
-        << "- Время парал. обр.: " << Time << " сек." << std::endl;
-
-
-    return 0;
+	for (int k = Chunk; k < X.size(); k++) {
+		Z[k - Chunk] = receive(buf2);
+		send(buf0, X[k]);
+	}
+	for (int k = X.size() - Chunk; k < X.size(); k++) {
+		Z[k] = receive(buf2);
+	}
+	Tms = (clock() - Tms) / CLOCKS_PER_SEC;
+	cout << "- Время параллельного алгоритма: " << Tms << " c." << endl;
 }
