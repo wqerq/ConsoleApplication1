ca4d5b28ccafa5fe0feafbb9c6eccb2da199a524
contest <contest@W12-414-0000006>
2024-11-22 14:38:37 +0400

    update

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,8 +2,10 @@
 #include <locale.h>
 #include <time.h>
 #include <vector>
+#include <deque>
 #include <ppl.h>
 #include <concurrent_vector.h>
+#include <concurrent_queue.h>
 #include <iostream>
 #include <cmath>
 #define N1 400 //Число узлов по каждому направлению
@@ -16,7 +18,7 @@ double Func(double x, double y)
 	double Tmp = 0;
 	for (int k = 1; k <= N2; k++)
 		for (int j = 1; j <= N2; j++)
-			Tmp += sin(k * x) * cos(j * y) / ((1 + k + j) * sqrt(1 + pow(k, 4) + pow(j, 4)));
+			Tmp += cos(k * x) * sin(j * y) / ((1 + k + j) * sqrt(1 + pow(k, 4) + pow(j, 4)));
 	return Tmp;
 }
 
@@ -32,6 +34,7 @@ struct Point
 int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, ".ACP");
+	std::cout << "Сравнение для векторов:" << std::endl;
 	std::vector<Point> Pts;
 	double h = 2.0 * M_PI / N1;
 	double Time = clock();
@@ -40,7 +43,7 @@ int main(int argc, char* argv[])
 			double x = h * k;
 			double y = h * j;
 			double f = Func(x, y);
-			if (f >= 0)
+			if (f <= 0)
 				Pts.push_back(Point(x, y, f));
 		}
 	Time = (clock() - Time) / CLOCKS_PER_SEC;
@@ -60,7 +63,40 @@ int main(int argc, char* argv[])
 			}}
 	);
 	Time = (clock() - Time) / CLOCKS_PER_SEC;
-	std::cout << "Вычисления завершены. Размер контейнера: " << CPts.size() << std::endl
+	std::cout << "Вычисления завершены. Размер вектора: " << CPts.size() << std::endl
 		<< "Время : " << Time << " сек." << std::endl;
 	CPts.clear();
+
+
+	std::cout << std::endl << "Сравнение для очередей:" << std::endl;
+	std::deque<Point> qPts;
+	h = 2.0 * M_PI / N1;
+	for (int k = 0; k < N1; k++)
+		for (int j = 0; j < N1; j++) {
+			double x = h * k;
+			double y = h * j;
+			double f = Func(x, y);
+			if (f <= 0)
+				Pts.push_back(Point(x, y, f));
+		}
+	Time = (clock() - Time) / CLOCKS_PER_SEC;
+	std::cout << "Вычисления завершены. Размер контейнера: " << Pts.size() << std::endl
+		<< "Время : " << Time << " сек." << std::endl;
+	Pts.clear();
+	concurrency::concurrent_queue<Point> CqPts;
+	Time = clock();
+	concurrency::parallel_for(0, N1,
+		[&CqPts, h](int k) {
+			for (int j = 0; j < N1; j++) {
+				double x = h * k;
+				double y = h * j;
+				double f = Func(x, y);
+				if (f >= 0)
+					CqPts.push(Point(x, y, f));
+			}}
+	);
+	Time = (clock() - Time) / CLOCKS_PER_SEC;
+	std::cout << "Вычисления завершены. Размер вектора: " << CqPts.unsafe_size() << std::endl
+		<< "Время : " << Time << " сек." << std::endl;
+	CqPts.clear();
 }
